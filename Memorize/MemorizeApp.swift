564461cc21fe2608879e8601b5b12a4e79ddb08c
Venkatesh nellore <51104553+samflynn007@users.noreply.github.com>
2024-07-13 20:26:33 +0530

    initial commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Venky on 13/07/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
